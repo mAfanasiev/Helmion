d419df5aca4c9cd48ea1becbc92bec0f0514d929
Vladislad T <v.trundenko@everad.com>
2018-12-18 12:50:07 +0200

    pravki vlad done

--- source/js/app.js
+++ source/js/app.js
@@ -283,50 +283,47 @@ $(document).ready(function() {
     });
 });
 
-(function($) {
-    $(window).on("load", function() {
-        $(".review-content").mCustomScrollbar({
+
+
+(function($){
+    $(window).resize(function(){
+        if($(this).width()>1191){
+             $(".review-content").mCustomScrollbar({
             callbacks:{
     onScrollStart:function(){
       $('.review-btn__list').addClass('all-rev-none')
     }
 }
         });
-    });
+        }else{
+           $('.review-content').mCustomScrollbar("disable"); 
+        }
+    }).trigger("resize");
+})(jQuery);
+
+(function($){
+    $(window).resize(function(){
+        if($(this).width()>1190){
+           click = 1;
+$('.review-btn__list2').click(function() {
+    if (click) {
+       $('.review-content').mCustomScrollbar("destroy"); 
+        click = 0;
+    } else {
+       $(".review-content").mCustomScrollbar();
+        click = 1;
+    }
+});
+        }else{
+            $(".review-content").mCustomScrollbar("destroy"); //destroy scrollbar 
+        }
+    }).trigger("resize");
 })(jQuery);
 
 
 
-if (window.matchMedia("(min-width: 1191px)").matches) {
-    click = 1;
-$('.review-btn__list').click(function() {
-    if (click) {
-        $(".review-content").mCustomScrollbar("disable");
-        click = 0;
-    } else {
-        $(".review-content").mCustomScrollbar("update");
-        click = 1;
-    }
-});
-} else {
-  /* the viewport is less than 400 pixels wide */
-}
 
-
-function myFunction(x) {
-  if (x.matches) { 
-    $(".review-content").mCustomScrollbar("disable");
-     $('.review-btn__list').addClass('dflex')
-  } else {
-    $(".review-content").mCustomScrollbar("update");
-    $('.review-btn__list').removeClass('dflex')
-  }
-}
-var x = window.matchMedia("(max-width: 1190px)")
-myFunction(x) 
-x.addListener(myFunction) 
 $('.review-btn__list').click(function() {
-    $('#mCSB_1_container').toggleClass('scrlt0');
     $('.review-content').toggleClass('review-scroll');
     $(this).toggleClass('review-btn__list-after');
     $(this).text($(this).text() == 'еще отзывы (13)' ? 'свернуть отзывы' : 'еще отзывы (13)');
@@ -383,12 +380,7 @@ $('.prezident__body').slick({
 
 
 
-$('.add-review__btn').click(function() {
-    $('.review-popup').removeClass('review-popup_hide');
-});
-$('.add-review__btn').click(function() {
-    $('.review-popup_last').addClass('review-popup_hide');
-});
+
 $('.review-popup_last__close').click(function() {
     $('.review-popup_last').removeClass('review-popup_hide');
 
@@ -415,4 +407,24 @@ $('.consist__open').click(function () {
     }
 
 
+});
+var reviewInput1 = $('.input__val-1');
+var reviewInput2 = $('.input__val-2');
+var reviewInput3 = $('.input__val-3');
+
+$('.add-review__btn').click(function () {
+    var reviewInput1 = $('.input__val-1').val().length,
+        reviewInput2 = $('.input__val-2').val().length,
+        reviewInput3 = $('.input__val-3').val().length;
+    if (reviewInput1 !== 0 && reviewInput2 !== 0 && reviewInput3 !== 0) {
+        $('.input__val-1').val("");
+        $('.input__val-2').val("");
+        $('.input__val-3').val("");
+        $('.review-popup_last').addClass('review-popup_hide');
+        $('.add-review__btn').click(function () {
+           
+        });
+    } else {
+        alert('Заполните поля');
+    }
 });
